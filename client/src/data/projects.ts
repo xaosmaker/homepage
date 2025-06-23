181877630661312ae8fc75d0886ed4520868a40a
Drosos <drososD@proton.me>
2025-06-23 15:23:04 +0300

    add the expence tracker on pages

--- client/src/data/projects.ts
+++ client/src/data/projects.ts
@@ -2,6 +2,7 @@ import type { ProjectType } from "../components/Project";
 
 export const projects: ProjectType[] = [
   { title: "My nginx server", photoSrc: "nginx3.png", photoAlt: "A photo of nginx server code", desc: "It's designed to streamline web app development and deployment with a focus on performance, flexibility, and maintainability. Read more on github", gitLink: "https://github.com/xaosmaker/nginx" },
-  { title: "My apps bundle", photoSrc: "myapps.png", photoAlt: "a photo of login page of my apps", desc: "This project started as a personal tool — something I built for myself to manage tasks and time more efficiently. Read more on github i have there and credential to check it", gitLink: "https://github.com/xaosmaker/myApps", liveLink: "https://apps.drosinakis.com/login" }
+  { title: "My apps bundle", photoSrc: "myapps.png", photoAlt: "a photo of login page of my apps", desc: "This project started as a personal tool — something I built for myself to manage tasks and time more efficiently. Read more on github i have there and credential to check it", gitLink: "https://github.com/xaosmaker/myApps", liveLink: "https://apps.drosinakis.com/login" },
+  { title: "Expense Tracker", desc: "This app will handle the expence of a user and show him what he pays. read more on github", gitLink: "https://github.com/xaosmaker/expenseTracker", photoSrc: "expenceTracker.png", photoAlt: "an image of code for expence tracker" }
 ]
 
